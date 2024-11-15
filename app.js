4a7d4abf4bcce041e5f96fc7fc987222134ccc28
Sakib Hassan <sakibhassan.webdev@gmail.com>
2024-11-15 17:02:34 +0600

    fix a bug

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ import bcrypt from "bcrypt";
 import cookieParser from "cookie-parser";
 import jwt from "jsonwebtoken";
 
+app.set("views", "./views");
 app.set("view engine", "ejs");
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
